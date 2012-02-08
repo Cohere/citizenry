c4fda1883a5691bed57328da295c78c2f734aebe
Mr. Matt <system9@composition9.com>
2012-02-08 13:49:47 -0700

    auth credentials hash nesting

--- config/initializers/_load_settings.rb
+++ config/initializers/_load_settings.rb
@@ -1,9 +1,8 @@
 # Load application settings from config/settings.yml
 settings_yml = HashWithIndifferentAccess.new(YAML.load_file(Rails.root.join('config', 'settings.yml')))
 
-settings_yml[:common][:auth_credentials] = []
 [:twitter, :facebook, :linked_in].each do |provider|
- settings_yml[:common][:auth_credentials][provider] = []
+ settings_yml[:common][:auth_credentials][provider] = {}
  [:key, :secret].each do |sub|
    settings_yml[:common][:auth_credentials][provider][sub] = ENV["#{provider}_#{sub}"]
  end
