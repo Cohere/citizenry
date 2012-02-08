c38259c0f9e000bcff0dfde8d5b94d97f586fea2
Mr. Matt <system9@composition9.com>
2012-02-08 16:53:51 -0700

    s3 config settings

--- config/initializers/_load_settings.rb
+++ config/initializers/_load_settings.rb
@@ -6,6 +6,9 @@ settings_yml = HashWithIndifferentAccess.new(YAML.load_file(Rails.root.join('con
    settings_yml[:common][:auth_credentials][provider][sub] = ENV["#{provider}_#{sub}"]
  end
 end
+[:access_key_id,:secret_access_key].each do |sub|
+  settings_yml[:common][:image_upload][:s3_credentials][sub] = ENV["s3_#{sub}"]
+end
 [:hoptoad,:secret_token,:session_cookie_key].each do |sub|
   settings_yml[:common][sub] = ENV[sub.to_s]
 end
