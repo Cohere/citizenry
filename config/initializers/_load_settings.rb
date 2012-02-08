0ed4d4ed38c9d83c1afd4e34744ea1bdbdc853b3
Mr. Matt <system9@composition9.com>
2012-02-08 12:47:06 -0700

    added settings config

--- config/initializers/_load_settings.rb
+++ config/initializers/_load_settings.rb
@@ -1,6 +1,15 @@
 # Load application settings from config/settings.yml
 settings_yml = HashWithIndifferentAccess.new(YAML.load_file(Rails.root.join('config', 'settings.yml')))
 
+[:twitter, :facebook, :linked_in].each do |provider|
+ [:key, :secret].each do |sub|
+   settings_yml[:common][:auth_credentials][provider][sub] = ENV["#{provider}_#{sub}"]
+ end
+end
+[:hoptoad,:secret_token,:session_cookie_key].each do |sub|
+   settings_yml[:common][sub] = ENV[sub.to_s]
+end
+
 merged_settings = settings_yml['common']
 merged_settings.deep_merge!(settings_yml[Rails.env]) if settings_yml.has_key?(Rails.env)
 
