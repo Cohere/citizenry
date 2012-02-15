0e7580f6a0aa2d2475eb8bf852eab9d41346b97a
Mr. Matt <system9@composition9.com>
2012-02-15 14:31:21 -0700

    removed domain from smtp settings

--- config/initializers/_load_settings.rb
+++ config/initializers/_load_settings.rb
@@ -12,7 +12,7 @@ end
 [:hoptoad,:secret_token,:session_cookie_key].each do |sub|
   settings_yml[:common][sub] = ENV[sub.to_s]
 end
-[:address,:domain,:port,:user_name,:password].each do |sub|
+[:address,:port,:user_name,:password].each do |sub|
   settings_yml['common']['mailer']['smtp_settings'][sub] = ENV["mailer_#{sub}"]
 end
 merged_settings = settings_yml['common']
