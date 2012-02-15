52e65a665861be475ce08fb8e64df52bd31a1697
Mr. Matt <system9@composition9.com>
2012-02-15 10:09:12 -0700

    mailer settings

--- config/initializers/_load_settings.rb
+++ config/initializers/_load_settings.rb
@@ -2,9 +2,9 @@
 settings_yml = HashWithIndifferentAccess.new(YAML.load_file(Rails.root.join('config', 'settings.yml')))
 
 [:twitter, :facebook, :linked_in].each do |provider|
- [:key, :secret].each do |sub|
-   settings_yml[:common][:auth_credentials][provider][sub] = ENV["#{provider}_#{sub}"]
- end
+  [:key, :secret].each do |sub|
+    settings_yml[:common][:auth_credentials][provider][sub] = ENV["#{provider}_#{sub}"]
+  end
 end
 [:access_key_id,:secret_access_key].each do |sub|
   settings_yml[:common][:image_upload][:s3_credentials][sub] = ENV["s3_#{sub}"]
@@ -12,7 +12,9 @@ end
 [:hoptoad,:secret_token,:session_cookie_key].each do |sub|
   settings_yml[:common][sub] = ENV[sub.to_s]
 end
-
+[:address,:domain,:port,:user_name,:password,:authentication,:enable_starttls_auto].each do |sub|
+  settings_yml[:common][:mailer][:smtp_settings][sub] = ENV["mailer_#{sub}"]
+end
 merged_settings = settings_yml['common']
 merged_settings.deep_merge!(settings_yml[Rails.env]) if settings_yml.has_key?(Rails.env)
 
